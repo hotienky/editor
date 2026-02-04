4cefa3422fd1a8c1c756bc30f2ea714e637323fb
Kindy <kyit1506@gmail.com>
2026-02-04 13:44:58 +0800

    细节调整

--- src/components/container/page.vue
+++ src/components/container/page.vue
@@ -214,6 +214,7 @@ watch(
     box-sizing: border-box;
     .umo-zoomable-content {
       margin: 0 auto;
+      overflow: hidden;
       box-shadow:
         rgba(0, 0, 0, 0.06) 0px 0px 10px 0px,
         rgba(0, 0, 0, 0.04) 0px 0px 0px 1px;
