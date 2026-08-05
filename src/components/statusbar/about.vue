5b045d2a4c6b50ddd4f03eda167a5a80fa6565fe
Kindy <kyit1506@gmail.com>
2026-08-05 10:22:38 +0700

    chore: rebrand to Kindy Editor
    - Update logo SVG with new Kindy Editor branding
    - Remove Umo Editor contributors from about dialog
    - Update welcome messages in en-US and zh-CN locales
    - Set watermark to disabled by default (empty text)
    - Update watermark default styling

--- src/components/statusbar/about.vue
+++ src/components/statusbar/about.vue
@@ -18,64 +18,14 @@
         padding: 30px 0 0;
       "
     >
-      <a href="https://www.umodoc.com" target="_blank">
-        <img src="@/assets/images/logo.svg" width="160" style="display: flex" />
-      </a>
+      <img src="@/assets/images/logo.svg" width="160" style="display: flex" />
       <p>
         {{ t('about.version') }}: v{{ version }} ({{ t('about.openSource') }})
       </p>
       <p v-html="t('about.poweredBy')"></p>
       <p>
-        <a href="https://github.com/umodoc/editor" target="_blank"> Github </a>
-        |
-        <a
-          :href="`https://dev.umodoc.com/${locale === 'zh-CN' ? 'cn' : 'en'}/docs/editor`"
-          target="_blank"
-        >
-          {{ t('about.documentation') }}
-        </a>
-        |
-        <a href="https://www.npmjs.com/package/@umoteam/editor" target="_blank">
-          NPM
-        </a>
+        <a href="https://github.com/hotienky/editor" target="_blank"> Github </a>
       </p>
-      <t-divider>{{ t('about.thanks') }}</t-divider>
-      <p style="text-align: center; font-size: 12px; padding: 0 10px">
-        <a href="https://github.com/Cassielxd" target="_blank">Cassielxd</a>
-        ·
-        <a href="https://github.com/china-wangxu" target="_blank"
-          >china-wangxu</a
-        >
-        ·
-        <a href="https://github.com/Goldziher" target="_blank"
-          >Na'aman Hirschfeld</a
-        >
-        ·
-        <a href="https://github.com/SevenDreamYang" target="_blank"
-          >SevenDreamYang</a
-        >
-        ·
-        <a href="https://github.com/ChenErik" target="_blank">ChenErik</a>
-        ·
-        <a href="https://github.com/SerRashin" target="_blank">SerRashin</a>
-        ·
-        <a href="https://github.com/SunnyWisozk" target="_blank"
-          >Sunny Wisozk</a
-        >
-        ·
-        <a href="https://github.com/xuzhenjun130k" target="_blank"
-          >Sherman Xu</a
-        >
-        ·
-        <a href="https://github.com/vace" target="_blank">vace</a>
-        ·
-        <a href="https://github.com/Mikasa33" target="_blank">Mikasa33</a>
-      </p>
-      <t-divider></t-divider>
-      <p
-        style="text-align: center; font-size: 12px"
-        v-html="t('about.next')"
-      ></p>
     </div>
   </modal>
 </template>
